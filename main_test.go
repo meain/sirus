08a5c7b126a863285e34317e0103a7dfbf5aa405
Abin Simon <mail@meain.io>
2022-02-19 12:47:46 +0530

    Add more tests

--- main_test.go
+++ main_test.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 )
+
+var url = "https://meain.io"
 
 func cleanDb() {
 	db = make(map[string]entry)
@@ -11,7 +17,6 @@ func cleanDb() {
 
 func TestGenShortcode(t *testing.T) {
 	cleanDb()
-	url := "https://domain.tld"
 	code := genCode(url)
 	if len(code) == 0 {
 		t.Error("got empty shortcode")
@@ -47,5 +52,77 @@ func TestGenShortcodeDuplicate(t *testing.T) {
 
 	if db[code].scount != 2 {
 		t.Errorf("invalid scount; expected '%v', got '%v'", 2, db[code].scount)
+	}
+}
+
+func TestSimpleGet(t *testing.T) {
+	cleanDb()
+	request, _ := http.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+	handler(response, request)
+	got := response.Body.String()
+
+	if got != "meain" {
+		t.Fatal("Did not get response back from server")
+	}
+}
+
+func TestCreateShort(t *testing.T) {
+	cleanDb()
+	request, _ := http.NewRequest(http.MethodPost, "/", bytes.NewReader([]byte(`{"url":"`+url+`"}`)))
+	response := httptest.NewRecorder()
+	handler(response, request)
+	got := response.Body.String()
+
+	if !strings.Contains(got, BASE_URL) {
+		t.Fatalf("%v not found in response", BASE_URL)
+	}
+}
+
+func TestCreateShortDuplicate(t *testing.T) {
+	cleanDb()
+	request, _ := http.NewRequest(http.MethodPost, "/", bytes.NewReader([]byte(`{"url":"`+url+`"}`)))
+	response := httptest.NewRecorder()
+	handler(response, request)
+	got := response.Body.String()
+
+	if !strings.Contains(got, BASE_URL) {
+		t.Fatalf("%v not found in response", BASE_URL)
+	}
+
+	request, _ = http.NewRequest(http.MethodPost, "/", bytes.NewReader([]byte(`{"url":"`+url+`"}`)))
+	response = httptest.NewRecorder()
+	handler(response, request)
+	got2 := response.Body.String()
+
+	if got != got2 {
+		t.Fatalf("short url not same; expected '%v', got '%v'", got, got2)
+	}
+}
+
+func TestRedirect(t *testing.T) {
+	cleanDb()
+	request, _ := http.NewRequest(http.MethodPost, "/", bytes.NewReader([]byte(`{"url":"`+url+`"}`)))
+	response := httptest.NewRecorder()
+	handler(response, request)
+	got := response.Body.String()
+
+	splits := strings.Split(got, "/")
+	code := splits[len(splits)-1]
+
+	request, _ = http.NewRequest(http.MethodGet, "/"+code, nil)
+	response = httptest.NewRecorder()
+	handler(response, request)
+
+	if response.Code != http.StatusTemporaryRedirect {
+		t.Errorf("expected redirect, got '%v'", response.Code)
+	}
+
+	loc, err := response.Result().Location()
+	if err != nil {
+		t.Fatal("no redirect location provided")
+	}
+	if loc.String() != url {
+		t.Errorf("incorrect redirect location; expected '%v', got '%v'", url, loc.String())
 	}
 }
